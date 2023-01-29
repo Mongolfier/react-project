13fa5d3fbd6201eb38a1e2fe8b60bc073380e891
Dmitry Mumrenkov <dimony4519@gmail.com>
2023-01-29 21:09:33 +0300

    decompose webpack config

---
+++ config/build/types/config.ts
@@ -0,0 +1,12 @@
+export type BuildMode = "development" | "production";
+export interface BuildPath {
+  entry: string;
+  build: string;
+  html: string;
+}
+
+export interface BuildOptions {
+  mode: BuildMode;
+  paths: BuildPath;
+  isDev: boolean;
+}
