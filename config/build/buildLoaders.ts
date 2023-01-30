25219d3a204c97e4107ee456c82a2f7dc53fd7ca
Dmitrij Mumrenkov <dmumrenkov@getmobit.ru>
2023-01-30 11:28:17 +0300

    add react, scss to webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,18 @@
 import webpack from "webpack"
 
 export const buildLoaders = (): webpack.RuleSetRule[] => {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      //Create 'style' nodes from JS strings
+      "style-loader",
+      // Translate CSS into CommonJS
+      "css-loader",
+      // Compile Sass to CSS
+      "sass-loader",
+    ]
+  }
+
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
@@ -9,5 +21,6 @@ export const buildLoaders = (): webpack.RuleSetRule[] => {
 
   return [
     typescriptLoader,
+    cssLoader,
   ]
 }
--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,15 +1,15 @@
-import path from "path";
 import webpack from "webpack";
+import { buildDevServer } from "./buildDevServer";
 import { buildLoaders } from "./buildLoaders";
 import { buildPlugins } from "./buildPlugins";
 import { buildResolvers } from "./buildResolvers";
 import { BuildOptions } from "./types/config"
 
 export const buildWebpackConfig = (options: BuildOptions): webpack.Configuration => {
-  const { mode, paths } = options;
+  const { mode, paths, isDev } = options;
 
   return {
-    mode: mode,
+    mode,
     entry: paths.entry,
     output: {
       filename: "[name].[contenthash].js",
@@ -21,5 +21,7 @@ export const buildWebpackConfig = (options: BuildOptions): webpack.Configuration
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
   }
 }
