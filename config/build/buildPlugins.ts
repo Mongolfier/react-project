9420971df5ef123a37babc96784988f019fb896f
Dmitry Mumrenkov <dimony4519@gmail.com>
2023-02-18 10:33:41 +0300

    add analyzer plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import HTMLWebpackPlugin from 'html-webpack-plugin'
 import { type BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => {
   const plugins = [
@@ -22,6 +23,9 @@ export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPlu
   if (isDev) {
     plugins.push(new ReactRefreshWebpackPlugin())
     plugins.push(new webpack.HotModuleReplacementPlugin())
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false
+    }))
   }
 
   return plugins
