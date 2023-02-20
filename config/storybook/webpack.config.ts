554d5f0f067d2338ed7964744f1e8e97058e0c3e
Dmitry Mumrenkov <dimony4519@gmail.com>
2023-02-20 23:38:19 +0300

    fix

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -11,23 +11,23 @@ export default ({ config }: { config: webpack.Configuration }) => {
     entry: '',
     src: path.resolve(__dirname, '..', '..', 'src')
   }
-  config.resolve.modules.push(paths.src)
-  config.resolve.extensions.push('.ts', '.tsx')
+  config.resolve?.modules?.push(paths.src)
+  config.resolve?.extensions?.push('.ts', '.tsx')
 
-  // eslint-disable-next-line no-param-reassign
-  config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
-    if ((rule.test as string).includes('svg')) {
-      return { ...rule, exclude: /\.svg$/i }
-    }
+  if ((config.module?.rules) != null) {
+    config.module.rules = config.module?.rules?.map((rule: RuleSetRule | '...') => {
+      if (rule !== '...' && (rule.test as string).includes('svg')) {
+        return { ...rule, exclude: /\.svg$/i }
+      }
+      return rule
+    })
+  }
 
-    return rule
-  })
-
-  config.module.rules.push({
+  config.module?.rules?.push({
     test: /\.svg$/,
     use: ['@svgr/webpack']
   })
-  config.module.rules.push(buildCssLoader(true))
+  config.module?.rules?.push(buildCssLoader(true))
 
   return config
 }
